4f55d4b97d5e6c7652d60973d213dfc18b3f8960
EC2 Default User <ec2-user@ip-172-31-30-154.us-east-2.compute.internal>
2021-02-21 04:23:42 +0000

    モデル実装、テストデータ作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts')
 
 @section('content')
-    <div class="container mt-4">  
+    <div class="container mt-4">
+        @foreach ($posts as $post)
         <div class="card mb-4">
             <div class="card-header mb-2">
-                Title
+                {{ $post->title }}
             </div>
             <div class="card-body">
                 <p class="card-text">
-                    body
+                    {{ $post->body }}
                 </p>
                 
             </div>
@@ -19,6 +20,7 @@
             </div>
 
         </div>
+        @endforeach
     </div>
     
 @endsection('content')
